a72eb14a8d0c734357098589e67d0d97a26c89ac
mmansoorali <m.mansoorali@gmail.com>
2015-02-04 13:42:44 +0600

    ffile f67

--- ffile1.java
+++ ffile1.java
@@ -1,4 +1,11 @@
 123------erum
+<!-- Author: Marina Pushkova (marina@githowto.com) -->
+<html>
+  <head>
+<<<<<<< HEAD
+    <link type="text/css" rel="stylesheet" media="all" href="style.css" />
+=======
+
 nihal
 nausheen------12321
 sohaib
